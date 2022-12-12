20404a954a4d572064f3d196e3d99e64d1e616e2
jackal.lch <jackal.lch@gmail.com>
2022-12-12 22:14:33 +0800

    build: basic setup and packages installed

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// INTERNAL IMPORT FOR NFT OPENZEPPLIN
+// Counters.sol: keep track the number of token
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
